564346e3bcb17ed395cd178633ee6b045640833c
Jake <monkeypsycho03@gmail.com>
2021-11-28 13:29:20 -0700

    moved rlock to own modual

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,24 @@
-use std::{collections::HashMap, sync::{RwLock, atomic::{AtomicU32, Ordering}}};
 #[macro_use]
 extern crate rocket;
-use rocket::{State, fs::{FileServer, relative}};
+use std::collections::HashMap;
+
+use rocket::fs::{FileServer, relative};
 
 use rocket_dyn_templates::{Template};
 
-// TODO turn into a request guard of sorts
-// https://rocket.rs/v0.5-rc/guide/state/
-struct Text {
-    text: RwLock<String>,
-}
+mod rwlock;
 
 #[get("/")]
 fn index() -> Template {
     Template::render("index", &HashMap::<&str, &str>::new())
 }
 
-#[get("/string")]
-fn string(text: &State<Text>) -> String {
-    format!("{}", text.text.read().expect("Oh shit"))
-}
-
-#[get("/string/<new>")]
-fn update(new: String, text: &State<Text>) -> String {
-    let mut old = text.text.write().unwrap();
-    *old = new;
-    format!("String is: {}", &old)
-}
-
-#[get("/user")]
-fn user() -> Template {
-    Template::render("user", &HashMap::<&str, &str>::new())
-}
-
-#[get("/admin")]
-fn admin() -> Template {
-    Template::render("admin", &HashMap::<&str, &str>::new())
-}
-
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, string, update, admin, user])
+        .mount("/", routes![index])
+        .mount("/rwlock/", routes![rwlock::index, rwlock::admin, rwlock::user, rwlock::update, rwlock::string])
         .mount("/", FileServer::from(relative!("static")))
-        .manage(Text {
-            text: RwLock::new(String::new()),
-        }).attach(Template::fairing())
+        .manage(rwlock::Text::new())
+        .attach(Template::fairing())
 }
---
+++ src/rwlock.rs
@@ -0,0 +1,43 @@
+use std::{collections::HashMap, sync::RwLock};
+
+use rocket::State;
+use rocket_dyn_templates::{Template};
+
+pub struct Text {
+    pub text: RwLock<String>,
+}
+
+impl Text{
+    pub fn new() -> Text{
+        Text {
+            text: RwLock::new(String::new())
+        }
+    }   
+}
+
+#[get("/")]
+pub fn index() -> Template {
+    Template::render("rwlock_index", &HashMap::<&str, &str>::new())
+}
+
+#[get("/string")]
+pub fn string(text: &State<Text>) -> String {
+    format!("{}", text.text.read().expect("Oh shit"))
+}
+
+#[get("/string/<new>")]
+pub fn update(new: String, text: &State<Text>) -> String {
+    let mut old = text.text.write().unwrap();
+    *old = new;
+    format!("String is: {}", &old)
+}
+
+#[get("/user")]
+pub fn user() -> Template {
+    Template::render("user", &HashMap::<&str, &str>::new())
+}
+
+#[get("/admin")]
+pub fn admin() -> Template {
+    Template::render("admin", &HashMap::<&str, &str>::new())
+}
